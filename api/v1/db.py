934fab239301cce0dd69c704703906e5d59573a7
Edris Ranjbar <edris.qeshm2@gmail.com>
2021-03-02 03:18:36 +0330

    Publisher model class added and Category updated (some code refactor)

---
+++ api/v1/db.py
@@ -0,0 +1,29 @@
+from psycopg2 import *
+from config import *
+
+connection = None
+cursor = None
+
+def do_connect():
+    global connection, cursor
+    try:
+        # Connect to an existing database
+        connection = connect(user=DB_USER,
+                             password=DB_PASSWORD,
+                             host=DB_HOST,
+                             port=DB_PORT,
+                             database=DB_NAME)
+        connection.set_session(autocommit=True)
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        # Print PostgreSQL details
+        print("PostgreSQL server information")
+        # Executing a SQL query
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
+
+def disconnect():
+    if connection:
+        cursor.close()
+        connection.close()
