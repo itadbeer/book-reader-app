b39d2dd2667cadd655612bd47298264658ea885e
Edris <edris.qeshm2@gmail.com>
2021-04-22 03:32:22 +0430

    fix AppBar margins

--- lib/screens/discovery/widgets/MyAppBar.dart
+++ lib/screens/discovery/widgets/MyAppBar.dart
@@ -11,14 +11,21 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       automaticallyImplyLeading: false,
       backgroundColor: Colors.white,
+      titleSpacing: 0.0,
       elevation: 0,
-      title: Text('کتاب خوان',
-          style: TextStyle(color: onSurfaceHighEmphasis, fontSize: 20)),
+      title: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Text('کتاب خوان',
+            style: TextStyle(color: onSurfaceHighEmphasis, fontSize: 20)),
+      ),
       actions: [
-        TextButton(
-            child: Text('ورود به حساب',
-                style: TextStyle(color: myTheme.primaryColor, fontSize: 16)),
-            onPressed: () {})
+        Container(
+          margin: EdgeInsets.only(left: 16),
+          child: TextButton(
+              child: Text('ورود به حساب',
+                  style: TextStyle(color: myTheme.primaryColor, fontSize: 16)),
+              onPressed: () {}),
+        )
       ],
     );
   }
