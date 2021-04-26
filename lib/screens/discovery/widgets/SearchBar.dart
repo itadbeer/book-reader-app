f2dec39206094680055cd15b7728e8237b62f89a
Edris <edris.qeshm2@gmail.com>
2021-04-26 06:03:19 +0430

    fix SearchBar UI implementation

--- lib/screens/discovery/widgets/SearchBar.dart
+++ lib/screens/discovery/widgets/SearchBar.dart
@@ -1,48 +1,71 @@
 import 'package:ibr/ibr.dart';
 
-class SearchBar extends StatelessWidget {
+class SearchBar extends StatefulWidget {
   const SearchBar({
     Key key,
   }) : super(key: key);
 
   @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  bool _showClearButton = false;
+  var _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      setState(() {
+        _showClearButton = _searchController.text.length > 0;
+      });
+    });
+  }
+
+  Widget _getClearButton() {
+    if (!_showClearButton) {
+      return null;
+    }
+    return IconButton(
+      onPressed: () => _searchController.clear(),
+      icon: Icon(Icons.clear),
+      color: onSurfaceMediumEmphasis,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var _searchController = TextEditingController();
     return Container(
         width: MediaQuery.of(context).size.width - 32,
+        height: 56,
         child: TextField(
-            controller: _searchController,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Color.fromRGBO(0, 0, 0, 0.04),
-              labelStyle: TextStyle(color: onSurfaceHighEmphasis, fontSize: 16),
-              hintStyle: TextStyle(color: onSurfaceDisabled, fontSize: 16),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(16)),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(16)),
-              hintText: 'جست و جو',
-              prefixIcon: Padding(
-                padding: const EdgeInsets.only(right: 27, left: 19),
-                child: Icon(
-                  Icons.search,
-                  color: onSurfaceDisabled,
-                ),
+          controller: _searchController,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color.fromRGBO(0, 0, 0, 0.04),
+            labelStyle: TextStyle(color: onSurfaceHighEmphasis, fontSize: 16),
+            hintStyle: TextStyle(color: onSurfaceDisabled, fontSize: 16),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(16)),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(16)),
+            hintText: 'جست و جو',
+            prefixIcon: Padding(
+              padding: const EdgeInsets.only(right: 27, left: 19),
+              child: Icon(
+                Icons.search,
+                color: onSurfaceDisabled,
               ),
-              prefixIconConstraints:
-                  BoxConstraints(minHeight: 24, minWidth: 24),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.all(21),
-                child: IconButton(
-                  onPressed: () => _searchController.clear(),
-                  icon: Icon(
-                    Icons.clear,
-                    color: onSurfaceMediumEmphasis,
-                  ),
-                ),
-              ),
-            )));
+            ),
+            prefixIconConstraints: BoxConstraints(minHeight: 24, minWidth: 24),
+            suffixIcon: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: _getClearButton(),
+            ),
+          ),
+        ));
   }
 }
