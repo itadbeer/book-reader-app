972bc1bd62f05bf8be741973c1e8c19668d70474
Edris <edris.qeshm2@gmail.com>
2021-05-15 12:12:45 +0430

    some unit tests are added

---
+++ test/api_test.dart
@@ -0,0 +1,17 @@
+import 'package:ibr/api/categories.dart';
+import 'package:ibr/models/book.dart';
+import 'package:ibr/models/category.dart';
+import 'package:test/test.dart';
+import 'package:ibr/api/books.dart';
+
+void main() {
+  test("get books from api", () async {
+    List<Book> books = await getBooks();
+    expect(books.first.id, 1);
+  });
+
+  test("get categories from api", () async {
+    List<Category> categories = await getCategories();
+    expect(categories.first.id, 1);
+  });
+}
--- test/json_test.dart
+++ test/json_test.dart
@@ -1,3 +1,4 @@
+import 'package:ibr/api/books.dart';
 import 'package:test/test.dart';
 import 'package:ibr/api/categories.dart';
 import 'package:ibr/api/category.dart';
@@ -31,5 +32,36 @@ void main() {
       }
     """;
     expect(parseCategory(jsonStr).id, 1);
+  });
+
+  test('parses books', () {
+    const jsonStr = """{
+    "results": [
+        {
+            "author_id": [
+                1
+            ],
+            "category_id": [
+                1,
+                2
+            ],
+            "description": "شرح عقیده صحاویه",
+            "discount_percentage": 0,
+            "id": 1,
+            "image": "test.png",
+            "name": "عقیده طحاویه",
+            "pages_count": 100,
+            "price": 0,
+            "publish_date": "1400/1/1",
+            "publisher_id": [
+                1
+            ],
+            "translator_id": [
+                1
+            ]
+        }
+    ]
+  }""";
+    expect(parseBooks(jsonStr).first.id, 1);
   });
 }
